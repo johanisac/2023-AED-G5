1361c6b463b0bb624c7a9d509166d7fca38e2905
johanisac <111624883+johanisac@users.noreply.github.com>
2023-04-02 23:30:38 -0500

    Delete LB02 directory

--- LB02/Actividades/TestGen.java
+++
@@ -1,9 +0,0 @@
-public class TestGen {
-    public static void main(String args[]) {
-        String[] v = { "Perez", "Sanchez", "Rodriguez" };
-        Integer[] w = { 12, 34, 56 };
-        System.out.println(exist(v, "Sanchez"));
-        System.out.println(exist(w, 34));
-        System.out.println(exist(w, "Salas")); // Error
-    }
-}
